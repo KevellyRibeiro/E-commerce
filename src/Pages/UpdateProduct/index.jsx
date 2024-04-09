d302afc3156469b5d7c003b37b9f638dc0f93e96
KevellyRibeiro <grupoargentes@gmail.com>
2024-04-08 21:18:04 -0300

    update Css

--- src/Pages/UpdateProduct/index.jsx
+++ src/Pages/UpdateProduct/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../UpdateProduct/style.css';
 
 const UpdateProduct = () => {
     const [Product, setProduct] = useState({
@@ -6,7 +7,8 @@ const UpdateProduct = () => {
         description: '',
         image: null,
         price: '',
-        category: ''
+        category: '',
+        tags: ''  // Adicionando tags ao estado
     });
 
     const addProduct = (e) => {
@@ -16,6 +18,7 @@ const UpdateProduct = () => {
         console.log(Product.description);
         console.log(Product.image);
         console.log(Product.price);
+        console.log(Product.tags);  // Adicionando log para tags
     }
 
     const handleInputChange = (e) => {
@@ -36,47 +39,92 @@ const UpdateProduct = () => {
 
     return (
         <>
-            <header>
-                <div>
-                    <h1>Bem vindo a sua página de criação de Produtos</h1>
-                </div>
-            </header>
             <section>
-                <form onSubmit={addProduct}>
-                    <label>Digite o título do seu produto!</label>
-                    <input 
-                        type="text" 
-                        name="title" 
-                        value={Product.title} 
-                        onChange={handleInputChange} 
-                    />
-                    
-                    <label>Digite a descrição do seu produto!</label>
-                    <input 
-                        type="text" 
-                        name="description" 
-                        value={Product.description} 
-                        onChange={handleInputChange} 
-                    />
-                    
-                    <label>Digite o Preço referente ao seu produto:</label>
-                    <input 
-                        type="number" 
-                        name="price" 
-                        value={Product.price} 
-                        onChange={handleInputChange} 
-                    />
-                    
-                    <label>Carregue sua Imagem!</label>
-                    <input 
-                        type="file" 
-                        name="image" 
-                        onChange={handleInputChange} 
-                    />
-                    
+                <div className='menuToogle'>
+                    <div><h3>Pagina Inicial</h3></div>
+                    <div><h3>Relatorios</h3></div>
+                    <div><h3>Produtos</h3></div>
+                    <div><h3>Ofertas</h3></div>
+                    <div></div>
+                </div>
+                <div className='containerFather'>
+                    <div className='containerHeader'>
+                    <h1>Create new product</h1>
+                     </div>
+                    <div className='containerForm'>
+                        <form onSubmit={addProduct} className='formProduct'>
+                        <label>Digite o título do seu produto!</label>
+                        <input 
+                            type="text" 
+                            name="title" 
+                            value={Product.title} 
+                            onChange={handleInputChange} 
+                            placeholder='Aspirador de pó'
+                        />
+                        <label>Digite a descrição do seu produto!</label>
+                        <input 
+                            type="text" 
+                            name="description" 
+                            value={Product.description} 
+                            onChange={handleInputChange} 
+                            placeholder='Cor vermelha com voltagem....'
+                            id='inputDesc'
+                        />
+                        <div>
+                            <label>Digite o Preço referente ao seu produto:</label>
+                            <input 
+                                type="number" 
+                                name="price" 
+                                value={Product.price} 
+                                onChange={handleInputChange} 
+                                placeholder='25,00 R$'
+                            />
+                        </div>
+                        <label>Carregue sua Imagem!</label>
+                        <input 
+                            type="file" 
+                            name="image" 
+                            onChange={handleInputChange} 
+                        />
+                        <label>Tags</label>
+                        <input 
+                            type="text" 
+                            name="tags"  // Corrigindo o nome para tags
+                            value={Product.tags}  // Associando ao estado
+                            onChange={handleInputChange} 
+                        />
+                        <label >Estoque</label>
+                        <input type="number" name="" id="inputEstoque" />
+                        <label >Categoria</label>
+                        <select name="" id="">
+                            <option value="eletronicos">Eletronicos</option>
+                            <option value="moda">
+                                Moda e Vestuario
+                            </option>
+                            <option value="decoracao">Casa e Decoração</option>
+                            <option value="beleza">Beleza e Cuidados Pessoais</option>
+                            <option value="esportes">Esportes e Fitness</option>
+                            <option value="livros">Livros e Midia</option>
+                            <option value="brinquedos">Brinquedos e Jogos</option>
+                            <option value="alimentos">Alimentos e Bebidas</option>
+                            <option value="moveis">Moveis</option>
+                            <option value="automotivo">Automotivo e Ferramentas</option>
+                            <option value="saude">Saúde</option>
+                            <option value="joias">Jóias e acessorios</option>
+                            <option value="pet">Pet</option>
+                            <option value="eletronicosHome">Eletronicos para casa</option>
+                            <option value="instrumentos">Instrumentos</option>
+                            <option value="papelaria">Papelaria</option>
+                            <option value="camping">Camping e Outdoor</option>
+                            <option value="festas">Festas e Eventos</option>
+                        </select>
+                        <input type="submit" value="Confirmar" />
 
-                    <input type="submit" value="Confirmar" />
-                </form>
+                        </form>
+
+                    </div>
+                </div>
+                
             </section>
         </>
     )
